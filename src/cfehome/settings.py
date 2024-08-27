73b728e32d469977a608cf23416d871ac7f8641d
marcelodiass <marcelodias.desenvolvedor@gmail.com>
2024-08-27 18:16:45 -0300

    flowbite settings

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -96,7 +96,7 @@ if DATABASE_URL is not None:
     'default': dj_database_url.config(
         default=DATABASE_URL,
         conn_health_checks=True,
-        conn_max_age=CONN_MAX_AGE    
+        conn_max_age=CONN_MAX_AGE
     )
 }
 
